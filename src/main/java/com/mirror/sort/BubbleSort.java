ce6a8ba52873e1e241aed26366facadf7efd3d0d
mirrordingjing <mirrordingjing@didichuxing.com>
2020-01-07 19:51:46 +0800

    bubble finished

--- src/main/java/com/mirror/sort/BubbleSort.java
+++ src/main/java/com/mirror/sort/BubbleSort.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
+ * 冒泡排序
  * Created by mirrordingjing @ 2020/1/7
  */
 public class BubbleSort<T> implements Sortable<T> {
@@ -12,14 +13,20 @@ public class BubbleSort<T> implements Sortable<T> {
     public T[] sort(T[] arr, Comparator<? super T> cmp) {
         int n=arr.length;
         for(int i=0;i<n-1;i++){
-            for(int j=i;j<n;j++){
-                if (cmp.compare(arr[i],arr[j])>0){
-                    T tmp=arr[i];
-                    arr[i]=arr[j];
+            //本次循环是否有过交换，无则表示已有序直接返回
+            boolean flag=false;
+            for(int j=0;j<n-1-i;j++){
+                if (cmp.compare(arr[j],arr[j+1])>0){
+                    T tmp=arr[j+1];
+                    arr[j+1]=arr[j];
                     arr[j]=tmp;
+                    flag=true;
                 }
             }
+            if(!flag){
+                return arr;
+            }
         }
-        return null;
+        return arr;
     }
 }
